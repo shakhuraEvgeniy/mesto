f8c85d7f099d95b4103610a77bf40b5fef522d1a
Evgeniy Shakhura <shah5@yandex.ru>
2022-03-15 23:51:19 +0300

    feat: remove card

--- scripts/index.js
+++ scripts/index.js
@@ -1,75 +1,86 @@
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
+let popup = document.querySelector(".popup");
+let formElement = popup.querySelector(".popup__form");
+const editName = document.querySelector(".profile__edit-button");
+const closePopup = popup.querySelector(".popup__close");
+let nameInput = popup.querySelector(".popup__input_type_name");
+let jobInput = popup.querySelector(".popup__input_type_job");
+let profileName = document.querySelector(".profile__name");
+let profilProfession = document.querySelector(".profile__profession");
+const cards = document.querySelector(".cards");
 
-
-let popup = document.querySelector('.popup');
-let formElement = popup.querySelector('.popup__form')
-const editName = document.querySelector('.profile__edit-button');
-const closePopup = popup.querySelector('.popup__close');
-let nameInput = popup.querySelector('.popup__input_type_name');
-let jobInput = popup.querySelector('.popup__input_type_job');
-let profileName = document.querySelector('.profile__name');
-let profilProfession = document.querySelector('.profile__profession');
-const cards = document.querySelector('.cards');
-
-function renderCard (card) {
+function renderCard(card) {
   const cardBlock = document
-  .querySelector(".card-template")
-  .content.firstElementChild.cloneNode(true);
+    .querySelector(".card-template")
+    .content.firstElementChild.cloneNode(true);
 
   cardBlock.querySelector(".card__caption").textContent = card.name;
   cardBlock.querySelector(".card__photo").setAttribute("src", card.link);
   cardBlock.querySelector(".card__photo").setAttribute("alt", card.name);
 
+  setCardActionsListener(cardBlock);
+
   cards.append(cardBlock);
 }
 
-function closedPopup () {
-  popup.classList.remove('popup_opened');
+function closedPopup() {
+  popup.classList.remove("popup_opened");
 }
 
-function openPopup () {
-  popup.classList.add('popup_opened');
+function openPopup() {
+  popup.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profilProfession.textContent;
 }
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profilProfession.textContent = jobInput.value;
   closedPopup();
 }
 
-editName.addEventListener('click', openPopup);
+function removeCard(event) {
+  const card = event.currentTarget.closest(".card");
+  card.remove();
+}
 
-closePopup.addEventListener('click', closedPopup);
+function setCardActionsListener(card) {
+  card.querySelector(".card__delete").addEventListener("click", removeCard);
+}
 
-formElement.addEventListener('submit', formSubmitHandler);
+document.body.addEventListener("click", (event) => {});
+
+editName.addEventListener("click", openPopup);
+
+closePopup.addEventListener("click", closedPopup);
+
+formElement.addEventListener("submit", formSubmitHandler);
 
 initialCards.map(renderCard);
