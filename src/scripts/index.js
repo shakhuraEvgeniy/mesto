51ab5951e83a7eb8ef80a33beac8fe4932b3e2a2
Evgeniy Shakhura <shah5@yandex.ru>
2022-05-25 12:03:23 +0300

    fix: after autotest 2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -69,9 +69,8 @@ function createCard(item, isDeleted, likes, setLike) {
       popupImage.open(evt.target.alt, evt.target.src);
     },
     handleLikeClick: (card, cardData) => {
-      const id = cardData._id;
       if (!card.querySelector('.card__heart_active')){
-        api.setLike(id)
+        api.setLike(cardData._id)
         .then((data)=>{
           card.querySelector('.card__heart').classList.add("card__heart_active");
           card.querySelector(".card__count-likes").textContent = data.likes.length;
@@ -80,7 +79,7 @@ function createCard(item, isDeleted, likes, setLike) {
           console.log(err);
         })
       } else {
-        api.removeLike(id)
+        api.removeLike(cardData._id)
         .then((data)=>{
           card.querySelector('.card__heart').classList.remove("card__heart_active");
           card.querySelector(".card__count-likes").textContent = data.likes.length;
